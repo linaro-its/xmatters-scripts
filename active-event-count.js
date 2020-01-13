9cad14bab1999f4c27d1071b324f1c9bac6fe62d
Philip Colmer <philip.colmer@linaro.org>
2020-01-13 11:34:18 +0000

    Use event property to search

--- active-event-count.js
+++ active-event-count.js
@@ -8,7 +8,7 @@
 // to this component.
 var request = http.request({ 
     "endpoint": "xMatters",
-    "path": "/api/xm/1/events?status=ACTIVE&embed=properties",
+    "path": "/api/xm/1/events?status=ACTIVE&embed=properties&propertyName=AffectedComponent&propertyValue="+encodeURIComponent(input["Affected Component"]),
     "method": "GET",
     "autoEncodeURI": false
 });
@@ -21,28 +21,7 @@ do {
     if (response.statusCode == 200 ) {
         var json = JSON.parse(response.body);
         console.log("Retrieved events: " + json.count);
-        var index;
-        for (index = 0; index < json.count; index++) {
-            // At the moment, need to process the CloudWatch Alarm Description
-            // the same way as the "Analyze CloudWatch Alarm" step does. This
-            // will hopefully be replaced by the ability to retrieve the
-            // affected component as a property of the event, thus only needing
-            // to divine the affected component once, and not in a step that is
-            // tied to the Status.io processes.
-            if ("AlarmDescription#en" in json.data[index].properties) {
-                var alarmDesc = json.data[index].properties["AlarmDescription#en"];
-                console.log("Alarm description: "+alarmDesc);
-                var res = alarmDesc.split("|");
-                var thisComponent = res[0].trim();
-                if (thisComponent == input["Affected Component"]) {
-                    console.log("Matched affected component");
-                    foundEvents++;
-                }
-            } else {
-                console.log("No alarm description property in event#"+index);
-            }
-        }
-
+        foundEvents += json.count;
         looping = ("next" in json.links);
         if (looping) {
             request.path = json.links.next;
